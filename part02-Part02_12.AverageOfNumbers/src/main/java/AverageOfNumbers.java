b433927ca069cf98e1cf19cacea01a0d1c0808f9
JesacaLin <JesacaLin@gmail.com>
2023-10-31 09:45:07 -0400

    initial commit

---
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == 0) {
+                break;
+
+            }
+            counter += 1;
+            sum += input;
+        }
+        double average = 1.0 * sum / counter;
+        System.out.println("Average of the numbers: " + average);
+        scanner.close();
+    }
+}
